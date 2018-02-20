2303345e16cab75e8c3a2bc12df143904419d568
blackscure <wekesabuyahi@gmail.com>
2018-02-20 18:12:06 +0300

    add data.py

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article one',
+            'body':'lorem avkjw jbdyvif khefygie kjeriug9r ijfrg98ru of79r ojfugowhua ihdfug98r jfeug',
+            'author':'Blas mone',
+            'create_date':'04-25-2018'
+        },
+
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'lorem avkjw jbdyvif khefygie kjeriug9r ijfrg98ru of79r ojfugowhua ihdfug98r jfeug',
+            'author': 'Blas jon',
+            'create_date': '04-25-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'lorem avkjw jbdyvif khefygie kjeriug9r ijfrg98ru of79r ojfugowhua ihdfug98r jfeug',
+            'author': 'Boow mone',
+            'create_date': '04-25-2018'
+        },
+
+     ]
+    return articles
